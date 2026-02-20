e34ed2f8cdca61c3e1fce65e253c8e27d0360f66
Sarthakkr04 <0231ite069@niet.co.in>
2026-02-20 23:35:47 +0530

    Time: 1168 ms (6.78%) | Memory: 70.4 MB (92.35%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -3,9 +3,7 @@ class Solution {
         int n = nums.length;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j <= i + k && j < n; j++) {
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
+                if (nums[i] == nums[j]) return true;
             }
         }
         return false;
